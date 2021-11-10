a5e6ece6fc1ca81522e5fdb9fda38a33df65145d
zhiqiang <1215017984@qq.com>
2021-11-10 19:59:44 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-21 20:47:50
- * @LastEditTime: 2021-11-01 22:53:57
+ * @LastEditTime: 2021-11-10 19:57:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \VUE\project\supermall\src\main.js
@@ -10,10 +10,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import FastClick from 'fastclick'
 Vue.config.productionTip = false
 
-FastClick.attch(document.body);
 Vue.prototype.$bus = new Vue()
 
 new Vue({
